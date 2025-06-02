7488a51d43eebafdd02905eaf155e946ab53d14f
Andrea <20230787@ricaldone.edu.sv>
2025-06-02 12:10:03 -0600

    funcion para agregar pendiente

--- js/script.js
+++ js/script.js
@@ -30,3 +30,56 @@ function mostrarDatos(datos){
 }
 
 obtenerPersonas();
+
+
+const modal = document.getElementById("modal-agregar");
+
+const btnAgregar = document.getElementById("btnAbrirModal");
+
+const btnCerrar = document.getElementById("btnCerrarModal");
+
+btnAgregar.addEventListener("click", ()  => {
+
+    modal.showModal();
+});
+
+
+btnCerrar.addEventListener("click", ()  => {
+
+    modal.close();
+});
+
+
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+
+    e.preventDefault();
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const edad = document.getElementById("edad").value.trim();
+
+    if(!nombre || !apellido || !email || !edad){
+        alert("Complete todos los campos");
+        return;
+    }
+
+    const respuesta = await fetch(API_URL, {
+        method:"POST",
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({nombre, apellido, email, edad})
+    });
+
+    if(respuesta.ok){
+        alert("El registro fue agregado correctamente")
+
+        
+        document.getElementById("frmAgregar").reset();
+        modal.close();
+        obtenerPersonas();
+    }
+    else{
+        alert("Hubo un error al agregar");
+    }
+
+});
